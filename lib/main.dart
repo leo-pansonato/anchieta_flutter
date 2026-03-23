62d0304e027af44f4037020c67aa43a0230d8305
Leonardo Capitani Pansonato <leonardo.pansonato@anchieta.br>
2026-03-22 23:40:32 -0300

    feat: implementar componentes para satisfazer os tests

--- lib/main.dart
+++ lib/main.dart
@@ -21,17 +21,109 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _controller = TextEditingController();
+  double _sliderValue = 0.5;
+  int _radioValue = 0;
+  bool _checkboxValue = false;
+  String? _resultado;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Olá Flutter')),
-        body: Center(
+    return Scaffold(
+      appBar: AppBar(title: Text('Olá Flutter')),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 8.0),
+        child: SingleChildScrollView(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 8,
             children: [
- 
+              Column(
+                children: [
+                  Text('Preencha com o nome do Produto:'),
+                  TextField(controller: _controller),
+                ],
+              ),
+              Column(
+                children: [
+                  Text('Defina a quantidade:'),
+                  Slider(
+                    value: _sliderValue,
+                    min: 0,
+                    max: 10,
+                    onChanged: (value) {
+                      setState(() {
+                        _sliderValue = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text('Selecione a forma de entrega:'),
+                  RadioGroup<int>(
+                    groupValue: _radioValue,
+                    onChanged: (value) {
+                      setState(() {
+                        _radioValue = value!;
+                      });
+                    },
+                    child: Column(
+                      children: [
+                        Row(children: [
+                          Radio<int>(value: 0),
+                          Text('Entrega'),
+                        ]),
+                        Row(children: [
+                          Radio<int>(value: 1),
+                          Text('Retirada'),
+                        ]),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text('Selecione uma opção:'),
+                  DropdownButton<String>(
+                    value: 'Opção 1',
+                    items: [
+                      DropdownMenuItem(value: 'Opção 1', child: Text('Opção 1')),
+                      DropdownMenuItem(value: 'Opção 2', child: Text('Opção 2')),
+                    ],
+                    onChanged: (value) {},
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text('Deseja receber promoções via e-mail?'),
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: _checkboxValue,
+                        onChanged: (value) {
+                          setState(() {
+                            _checkboxValue = value!;
+                          });
+                        },
+                      ),
+                      Text('Sim, eu desejo receber promoções via e-mail!'),
+                    ],
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _resultado = _controller.text;
+                    _controller.clear();
+                  });
+                },
+                child: Text('Cadastrar'),
+              ),
+              if (_resultado != null) Text(_resultado!),
             ],
           ),
         ),
